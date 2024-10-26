6e14e7ba782accb568c5a0fefaf449441e3b854d
Lancelot258 <83276993+Lancelot258@users.noreply.github.com>
2024-10-26 17:09:12 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+int main(int argc, char* argv[]) {
+	string ioDir;
+    if (argc == 1) {
+        cout << "Enter path containing the memory files: ";
+        cin >> ioDir;
+    }
+    else if (argc > 2) {
+        cout << "Invalid number of arguments. Machine stopped." << endl;
+        return -1;
+    }
+    else {
+        ioDir = argv[1];
+        cout << "IO Directory: " << ioDir << endl;
+    }
+    InsMem imem = InsMem("Imem", ioDir);
+    DataMem dmem_ss = DataMem("SS", ioDir);
+	DataMem dmem_fs = DataMem("FS", ioDir);
+
+	SingleStageCore SSCore(ioDir, &imem, &dmem_ss);
+	FiveStageCore FSCore(ioDir, &imem, &dmem_fs);
+
+    while (1) {
+		if (!SSCore.halted)
+			SSCore.step();
+
+		if (!FSCore.halted)
+			FSCore.step();
+
+		if (SSCore.halted && FSCore.halted)
+			break;
+    }
+
+	// dump SS and FS data mem.
+	dmem_ss.outputDataMem();
+	dmem_fs.outputDataMem();
+
+	return 0;
+}
